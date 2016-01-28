3dfab3f2096d46ebe8d58b6b72983f0c35433a51
PratikPatel13 <PratikPatel13@missionrnd.com>
2016-01-28 22:20:00 +0530

    Submit Commit

--- src/insertAtEveryKthNode.cpp
+++ src/insertAtEveryKthNode.cpp
@@ -13,12 +13,38 @@ NOTES:
 
 #include <stdio.h>
 #include <malloc.h>
-
+struct node * createNode(int num);
 struct node {
 	int num;
 	struct node *next;
 };
 
 struct node * insertAtEveryKthNode(struct node *head, int K) {
-	return NULL;
+	if (head == NULL || K <= 0)
+		return NULL;
+	struct node *current = head, *newNode = NULL;
+	int count = 1;
+	while (current != NULL)
+	{
+		if (count % K == 0)
+		{
+			newNode = createNode(K);
+			newNode->next = current->next;
+			current->next = newNode;
+			current = newNode->next;
+		}
+		else
+		{
+			current = current->next;
+		}
+		count++;
+	}
+	return head;
+}
+
+struct node * createNode(int num) {
+	struct node *newNode = (struct node *)malloc(sizeof(struct node));
+	newNode->num = num;
+	newNode->next = NULL;
+	return newNode;
 }
